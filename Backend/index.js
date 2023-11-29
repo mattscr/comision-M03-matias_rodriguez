d59930ce97d4cc2b956857f6a072f201caeac84a
Matt <matias.rodriguez.e@gmail.com>
2023-11-29 07:36:40 -0300

    desarrollo de index.js y envexample

---
+++ Backend/index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import { settingDotEnv } from "./src/settings/config.js";
+import { connectionMongodb } from "./src/settings/db.config.js";
+import authRoutes from "./src/routes/auth.routes.js";
+import postRouter from "./src/routes/post.routes.js";
+import userRouter from "./src/routes/user.routes.js";
+import cors from "cors";
+import helmet from "helmet";
+//app
+const app = express();
+
+//PORT
+const { port } = settingDotEnv();
+
+//MIDDLEWARE
+app.use(morgan("dev"));
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//ROUTES
+app.get("/", (req, res) => {
+  res.send("<h2>Pagina principal!</h2>");
+});
+app.use("/auth", authRoutes);
+app.use("/post", postRouter);
+app.use("/users", userRouter);
+
+//si no encuentra ninguna ruta: 404 not found
+app.use((req, res) => {
+  res.send("<h2>404</h2>");
+});
+
+app.listen(port, async () => {
+  await connectionMongodb();
+  console.log(`🚀servidor corriendo en http://localhost:${port}`);
+});
