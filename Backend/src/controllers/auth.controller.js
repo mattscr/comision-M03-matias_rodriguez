2f950ba30b1ca2870baa4c8f0584c58ec0197f36
Matt <matias.rodriguez.e@gmail.com>
2023-11-29 07:47:18 -0300

    controller de post y registro de usuarios

---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,92 @@
+import { userModel } from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import { createAccesToken } from "../middlewares/jsonwebtoken.js";
+import { settingDotEnv } from "../settings/config.js";
+import jwt from "jsonwebtoken";
+
+export const registerController = async (req, res) => {
+  //capturamos los datos del body
+  const { username, email, password, avatarURL } = req.body;
+  try {
+    //encriptar contraseña
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    //creamos un usuario nuevo con los datos capturados
+    const NewUser = new userModel({
+      username,
+      email,
+      password: passwordHash,
+      avatarURL,
+    });
+    //guardamos el nuevo usuario
+    const UserSaved = await NewUser.save();
+    //creamos access token
+    const token = await createAccesToken({ id: UserSaved._id });
+    res.cookie("token", token);
+    res.json({
+      message: "Usuario registrado con éxito",
+      id: UserSaved.id,
+      username: UserSaved.username,
+      email: UserSaved.email,
+    });
+  } catch (error) {
+    //en caso de falla, respondemos con un status code y un mensaje
+    res.status(500).json({ message: "error al registrar el usuario", error });
+  }
+};
+
+export const loginController = async (req, res) => {
+  //capturamos los datos del body
+  const { username, email, password, avatarURL } = req.body;
+  try {
+    //verificamos si se encuentra el usuario por email
+    const UserFound = await userModel.findOne({ email });
+    if (!UserFound)
+      return res.status(400).json({ message: "Usuario no encontrado" });
+
+    //verificamos si la contraseña es correcta
+    const match = await bcrypt.compare(password, UserFound.password);
+    if (!match)
+      return res.status(400).json({ message: "Contraseña incorrecta" });
+
+    //generamos el token de acceso
+    const token = await createAccesToken({ id: UserFound._id });
+    res.cookie("token", token);
+    res.json({
+      message: "Bienvenido!",
+      username: UserFound.username,
+      email: UserFound.email,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error al loguearse", error });
+  }
+};
+
+export const logoutController = async (req, res) => {
+  //se limpian las cookies
+  res.cookie("token", "", { expires: new Date(0) });
+  return res.status(200).json({ message: "Ha cerrado exitosamente la sesión" });
+};
+
+const { secret_key } = settingDotEnv();
+
+export const verifyToken = async (req, res) => {
+  const { token } = req.cookies;
+
+  console.log("asd: ", token);
+
+  if (!token) return res.status(401).json({ message: "No autorizado" });
+
+  jwt.verify(token, secret_key, async (err, user) => {
+    if (err) return res.status(401).json({ message: "No autorizado" });
+
+    const userFound = await userModel.findById(user.id);
+    if (!userFound) return res.status(401).json({ message: "No autorizado" });
+
+    return res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+    });
+  });
+};
---
+++ Backend/src/controllers/post.controller.js
@@ -0,0 +1,61 @@
+import { postModel } from "../models/post.model.js";
+
+export const createController = async (req, res) => {
+  try {
+    const newPost = await postModel.create(req.body);
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(500).json({ message: "error al crear post", error });
+  }
+};
+
+export const listController = async (req, res) => {
+  try {
+    const allPost = await post.find({}, "-__v");
+    console.log(allPost);
+
+    if (allPost.length < 1)
+      return res.status(204).json({ message: "no existen post" });
+
+    res.json(allPost);
+  } catch (error) {
+    res.status(500).json({ message: "error al mostrar post", error });
+  }
+};
+
+export const byIdController = async (req, res) => {
+  //guardamos el id en una constante
+  const { id } = req.params;
+  try {
+    //nos devueve el post que coincide con la id y la guardamos en una constante
+    const postId = await postModel.findById(id);
+    if (!postId) return res.status(404).json({ message: "post no encontrado" });
+    //mostramos el post obtenido
+    res.json(postId);
+  } catch (error) {
+    //si falla muestra el error
+    res.status(500).json({ message: "error al obtener post", error });
+  }
+};
+
+export const updateController = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updatedPost = await postModel.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(updatedPost);
+  } catch (error) {
+    res.status(500).json({ message: "no se pudo actualizar post", error });
+  }
+};
+
+export const deleteController = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedPost = await postModel.findByIdAndDelete(id);
+    res.json(deletedPost);
+  } catch (error) {
+    res.status(500).json({ message: "no se pudo eliminar", error });
+  }
+};
